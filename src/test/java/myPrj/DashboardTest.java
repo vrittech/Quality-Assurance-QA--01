ebb2f39d0cd8bc326962e8aeff0a45bc9143b77a
Lav Thaiba <lavthaiba@gmail.com>
2024-03-23 20:15:54 +0545

    using instructor email

--- src/test/java/myPrj/DashboardTest.java
+++ src/test/java/myPrj/DashboardTest.java
@@ -1,5 +1,7 @@
 package myPrj;
 
+import static org.testng.Assert.assertEquals;
+
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 import pagerepo.DashboardPage;
@@ -8,36 +10,132 @@ import pagerepo.LoginPage;
 public class DashboardTest extends BaseClass{
 	
 	
-	@Test(priority=20)
+	/*
+	 * 
+	 * @Test(priority=20)
+	 * 
+	 * public void verifyLoginStatusText() {
+	 * 
+	 * LoginPage login = new LoginPage(driver); login.callLogin();
+	 * 
+	 * DashboardPage dashboardItem = new DashboardPage(driver); String
+	 * loginStatusText =dashboardItem.loginInStatusText().getText();
+	 * if(dashboardItem.loginInStatusText().isDisplayed()) {
+	 * System.out.println("Login Status text is : "+loginStatusText);
+	 * Reporter.log("==Login Status Text shown and is Verified!", true); } }
+	 * 
+	 * 
+	 * @Test(priority=30)
+	 * 
+	 * public void verifyGreetingText() {
+	 * 
+	 * LoginPage login = new LoginPage(driver); login.callLogin();
+	 * 
+	 * DashboardPage dashboardItem = new DashboardPage(driver); String greetingText
+	 * = dashboardItem.greetingText().getText();
+	 * if(dashboardItem.greetingText().isDisplayed()) {
+	 * System.out.println("Greeting text is : "+ greetingText);
+	 * Reporter.log("==Greeting Text is verified!", true); } }
+	 * 
+	 * @Test(priority=40) public void verifyTotalApplicants() {
+	 * 
+	 * LoginPage login = new LoginPage(driver); login.callLogin();
+	 * 
+	 * DashboardPage dashboardItem = new DashboardPage(driver); String
+	 * totalApplicantsActualText = dashboardItem.totalApplicants().getText(); String
+	 * totalApplicantsExpectedText = "TOTAL APPLICANTS";
+	 * assertEquals(totalApplicantsActualText, totalApplicantsExpectedText);
+	 * Reporter.log("==Total Applicants text verified!==", true); }
+	 * 
+	 * 
+	 * @Test(priority=50) public void verifyTotalApplicantsNumber() {
+	 * 
+	 * LoginPage login = new LoginPage(driver); login.callLogin();
+	 * 
+	 * DashboardPage dashboardItem = new DashboardPage(driver); String
+	 * totalApplicantsNumberTxt = dashboardItem.totalApplicantsNumber().getText();
+	 * if(dashboardItem.totalApplicantsNumber().isDisplayed()) { System.out.
+	 * println("Total Applicants Number displayed with the total Applicants Number of : "
+	 * + totalApplicantsNumberTxt);
+	 * Reporter.log("==Total Applicants Number Verified!==", true); } }
+	 * 
+	 * 
+	 * @Test(priority=60) public void verifyTotalApplications() {
+	 * 
+	 * LoginPage login = new LoginPage(driver); login.callLogin();
+	 * 
+	 * DashboardPage dashboardItem = new DashboardPage(driver); String
+	 * totalApplicationsActualText = dashboardItem.totalApplications().getText();
+	 * String totalApplicationsExpectedText = "TOTAL APPLICATIONS";
+	 * assertEquals(totalApplicationsActualText, totalApplicationsExpectedText);
+	 * Reporter.log("==Total Applications Text has been Verified!==", true);
+	 * 
+	 * }
+	 */
 	
-	public void verifyLoginStatusText() {
+	/*
+	 * @Test(priority=70) public void verifyTotalApplicationsNumber() {
+	 * 
+	 * LoginPage login = new LoginPage(driver); login.callLogin();
+	 * 
+	 * DashboardPage dashboardItem = new DashboardPage(driver); String
+	 * totalApplicationsNumberTxt =
+	 * dashboardItem.totalApplicationsNumber().getText();
+	 * if(dashboardItem.totalApplicationsNumber().isDisplayed()) { System.out.
+	 * println("Total Applications Number is verified with the number of : "
+	 * +totalApplicationsNumberTxt);
+	 * Reporter.log("==Total Applications Number has been Verified!==", true); } }
+	 */
+	/*
+	 * @Test(priority=80) public void verifyGrantedStudent() {
+	 * 
+	 * LoginPage login = new LoginPage(driver); login.callLogin();
+	 * 
+	 * DashboardPage dashboardItem = new DashboardPage(driver); String
+	 * grantedStudentActualText = dashboardItem.grantedStudent().getText(); String
+	 * grantedStudentExpectedText = "GRANTED STUDENTS";
+	 * assertEquals(grantedStudentActualText, grantedStudentExpectedText);
+	 * Reporter.log("==Granted Student Text has been Verified!==", true); }
+	 * 
+	 * @Test(priority=90) public void verifyGrantedStudentNumber() {
+	 * 
+	 * LoginPage login = new LoginPage(driver); login.callLogin();
+	 * 
+	 * DashboardPage dashboardItem = new DashboardPage(driver); String
+	 * grantedStudentNumberTxt = dashboardItem.grantedStudentNumber().getText();
+	 * if(dashboardItem.grantedStudentNumber().isDisplayed()) {
+	 * System.out.println("Granted Student Number is Verified with the number of :"
+	 * +grantedStudentNumberTxt);
+	 * Reporter.log("==Granted Student Number has been Verfied!==", true); }
+	 * 
+	 * }
+	 */
+	
+	@Test(priority=100)
+	public void verifyTotalUniversities() {
 		
 		LoginPage login = new LoginPage(driver);
 		login.callLogin();
 		
 		DashboardPage dashboardItem = new DashboardPage(driver);
-		String loginStatusText =dashboardItem.loginInStatusText().getText();
-		if(dashboardItem.loginInStatusText().isDisplayed()) {
-			System.out.println("Login Status text is : "+loginStatusText);
-			Reporter.log("==Login Status Text shown and is Verified!", true);
-		}
+		String totalUniversitiesActualText = dashboardItem.totalUniversties().getText();
+		String totalUniversitiesExpectedText = "TOTAL UNIVERSITIES";
+		assertEquals(totalUniversitiesActualText, totalUniversitiesExpectedText);
+		Reporter.log("==Total Universities Text has been Verified!", true);
 	}
-
 	
-	@Test(priority=30)
-	
-	public void verifyGreetingText() {
+	@Test(priority=110)
+	public void verifyTotalUniversitiesNumber() {
 		
 		LoginPage login = new LoginPage(driver);
 		login.callLogin();
 		
 		DashboardPage dashboardItem = new DashboardPage(driver);
-		String greetingText = dashboardItem.greetingText().getText();
-		if(dashboardItem.greetingText().isDisplayed()) {
-			System.out.println("Greeting text is : "+ greetingText);
-			Reporter.log("==Greeting Text is verified!", true);
+		String totalUniversitiesTxt = dashboardItem.totalUniversitiesNumber().getText();
+		if(dashboardItem.totalUniversitiesNumber().isDisplayed()) {
+			System.out.println("Total Universities Number is verified with the number of :"+ totalUniversitiesTxt);
+			Reporter.log("==Total Universities Number has been Verified!==", true);
 		}
 	}
-	
 
 }
--- src/test/java/pagerepo/DashboardPage.java
+++ src/test/java/pagerepo/DashboardPage.java
@@ -11,6 +11,14 @@ public class DashboardPage {
 	private By dashboardLinkText = By.xpath("//span[text()=' Dashboard']");
 	private By loginInStatusText = By.xpath("//div[@class='container px-4 mt-12 mx-auto ']/descendant::div[1]");
 	private By greetingText = By.xpath("//div[@class='container px-4 mt-12 mx-auto ']/descendant::div[2]");
+	private By totalApplicants = By.xpath("(//div[@class='text-neutral-400 uppercase text-label-lg'])[1]");
+	private By totalApplicantsNumber = By.xpath("//div[@class='text-primary-500 font-bold  text-heading-md']");
+	private By totalApplications = By.xpath("(//div[@class='text-neutral-400 uppercase text-label-lg'])[2]");
+	private By totalApplicationsNumber = By.xpath("//div[@class='text-accent-500 font-bold  text-heading-md']");
+	private By grantedStudent = By.xpath("(//div[@class='text-neutral-400 uppercase text-label-lg'])[3]");
+	private By grantedStudentNumber = By.xpath("//div[@class='text-success-500 font-bold  text-heading-md']");
+	private By totalUniversities = By.xpath("(//div[@class='text-neutral-400 uppercase text-label-lg'])[4]");
+	private By totalUniversitiesNumber = By.xpath("//div[@class='text-[#FEA501] font-bold  text-heading-md']");
 	
 	public DashboardPage(WebDriver driver){
 		
@@ -32,6 +40,46 @@ public class DashboardPage {
 	public WebElement greetingText() {
 		
 		return driver.findElement(greetingText);
+	}
+	
+	public WebElement totalApplicants() {
+		
+		return driver.findElement(totalApplicants);
+	}
+	
+	public WebElement totalApplicantsNumber() {
+		
+		return driver.findElement(totalApplicantsNumber);
+	}
+	
+	public WebElement totalApplications() {
+		
+		return driver.findElement(totalApplications);
+	}
+	
+	public WebElement totalApplicationsNumber() {
+		
+		return driver.findElement(totalApplicationsNumber);
+	}
+	
+	public WebElement grantedStudent() {
+		
+		return driver.findElement(grantedStudent);
+	}
+	
+	public WebElement grantedStudentNumber() {
+		
+		return driver.findElement(grantedStudentNumber);
+	}
+	
+	public WebElement totalUniversties() {
+		
+		return driver.findElement(totalUniversities);
+	}
+	
+	public WebElement totalUniversitiesNumber() {
+		
+		return driver.findElement(totalUniversitiesNumber);
 	}
 
 }
